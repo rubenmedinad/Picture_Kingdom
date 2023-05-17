53adbc80199d0ee31ed2d896d22094f8d8542f84
FernanVaz <130450605+FernanVaz@users.noreply.github.com>
2023-05-17 17:01:47 +0200

    Cartelera
    Ya la cartelera ya pilla la id

--- src/app/sinopsis/sinopsis.component.ts
+++ src/app/sinopsis/sinopsis.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { pelicula } from '../_Modules/pelicula';
 import { PictureKingdomService } from '../picture-kingdom.service';
+import { ActivatedRoute, Router } from '@angular/router';
 @Component({
   selector: 'app-sinopsis',
   templateUrl: './sinopsis.component.html',
@@ -9,12 +10,16 @@ import { PictureKingdomService } from '../picture-kingdom.service';
 export class SinopsisComponent {
   peliculas: pelicula[] = []
   ide:number = 0
-  constructor(private peliculasS:PictureKingdomService){
+  constructor(private peliculasS:PictureKingdomService, private activarrutas: ActivatedRoute,
+    private rutes: Router){
     this.peliculas = peliculasS.getPeliculas()
   }
   pelicula :pelicula= new pelicula(0,"","","","",0,"",0,"")
-  
+
   ngOnInit(){
+    this.activarrutas.params.subscribe(data => {
+      this.ide = data['id'];
+    })
     this.pelicula=this.peliculas[this.ide]
   }
 
