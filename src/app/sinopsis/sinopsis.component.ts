eb7bf213a8b7eaa4d5cb71f3c881a78173178a90
ismaelpenalvafernandez <ismael.penalva@softtek.com>
2023-05-22 10:43:25 +0200

    sinopsisend
    sinopsis finalizado

--- src/app/sinopsis/sinopsis.component.ts
+++ src/app/sinopsis/sinopsis.component.ts
@@ -12,16 +12,10 @@ export class SinopsisComponent {
   peliculas: pelicula[] = [];
   ide: number = 0;
   pelicula: pelicula = new pelicula(0, "", "", "", "", "", "", "", "");
-  DiasPeliculas: string[] = ["Hoy", "Lun 1 May", "Mar 2 May", "Mié 3 May", "Jue 4 May", "Vie 5 May"];
-  HorariosPorDia: { [key: string]: string[] } = {
-    "Hoy": ["16:00 PM", "17:00 PM", "18:00 PM", "20:00 PM", "22:00 PM", "24:00 PM"],
-    "Lun 1 May": ["10:00 AM", "12:00 PM", "14:00 PM", "16:00 PM"],
-    "Mar 2 May": ["11:00 AM", "13:00 PM", "15:00 PM", "17:00 PM"],
-    "Mié 3 May": ["09:00 AM", "12:00 PM", "15:00 PM"],
-    "Jue 4 May": ["14:00 PM", "16:00 PM", "18:00 PM"],
-    "Vie 5 May": ["10:00 AM", "12:00 PM", "14:00 PM", "16:00 PM", "18:00 PM"]
-  };
-  diaSeleccionado: string = "";
+  diasPeliculas: number[] = [];
+  horariosPorDia: { [key: number]: string[] } = {};
+
+  diaSeleccionado: number = 0;
 
   constructor(
     private peliculasS: PictureKingdomService,
@@ -36,13 +30,42 @@ export class SinopsisComponent {
       this.ide = data['id'];
     });
     this.pelicula = this.peliculas[this.ide - 1];
+
+    this.generarFechas();
   }
 
-  cambiarDiaSeleccionado(dia: string) {
+  generarFechas() {
+    const fechaActual = new Date();
+    this.diasPeliculas = Array.from({ length: 6 }, (_, i) => i);
+    
+    const opcionesFecha = { weekday: 'short', month: 'long', day: 'numeric' };
+
+    this.horariosPorDia = {
+      0: ["16:00 PM", "17:00 PM", "18:00 PM", "20:00 PM", "22:00 PM", "24:00 PM"],
+      1: ["17:00 AM", "18:00 PM", "19:00 PM", "20:00 PM"],
+      2: ["17:00 AM", "19:00 PM", "20:00 PM", "21:00 PM"],
+      3: ["16:00 AM", "18:00 PM", "20:00 PM"],
+      4: ["18:00 PM", "20:00 PM", "22:00 PM"],
+      5: ["16:00 AM", "18:00 PM", "20:00 PM", "22:00 PM", "24:00 PM"]
+    };
+  }
+
+  cambiarDiaSeleccionado(dia: number) {
     this.diaSeleccionado = dia;
   }
 
-  getHorariosPorDia(dia: string): string[] {
-    return this.HorariosPorDia[dia] || [];
+  getHorariosPorDia(dia: number): string[] {
+    return this.horariosPorDia[dia] || [];
+  }
+
+  obtenerFechaActual(dia: number): string {
+    const fechaActual = new Date();
+    fechaActual.setDate(fechaActual.getDate() + dia);
+    const opcionesFecha: Intl.DateTimeFormatOptions = {
+      weekday: 'short',
+      month: 'long',
+      day: 'numeric'
+    };
+    return fechaActual.toLocaleDateString('es-ES', opcionesFecha);
   }
 }
