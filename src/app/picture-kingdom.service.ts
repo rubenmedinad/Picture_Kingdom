0d69d393dc3e1e123fec07479c55a62200adefdf
rubenmedinad <130450191+rubenmedinad@users.noreply.github.com>
2023-05-27 16:30:53 +0200

    asdfas
    dasdfas

--- src/app/picture-kingdom.service.ts
+++ src/app/picture-kingdom.service.ts
@@ -12,21 +12,21 @@ export class PictureKingdomService {
     { name: 'Cartelera', route: '/cartelera' },
     { name: 'Perfil', route: '/editarperfil' }
   ];
-  
+
   entradas: any[] = [
     { nombreEntrada: 'Normal', precio: 8.90, numero: 0 },
     { nombreEntrada: 'Abono Joven', precio: 6.50, numero: 0 },
     { nombreEntrada: 'Familia Numerosa', precio: 6.50, numero: 0 }
   ];
-  
+
   pelis = [
     { titulo: 'Inicio', route: '/' },
     { name: 'Cartelera', route: '/cartelera' },
     { name: 'Contact', route: '/contact' }
   ];
-  
+
   ventas: any[] = [];
-  
+
   peliculas: Peliculas[] = [
     new Peliculas(1, "Super Mario Bros: La película", 'Adaptación de la serie de videojuegos de Nintendo. La película cuenta la historia de Mario y Luigi, dos hermanos que viajan a un mundo oculto para rescatar a la Princesa Peach, capturada por el malvado Rey Bowser. Las cosas, sin embargo no serán sencillas. Mario y Luigi tendrán que enfrentarse a un ejército de setas animadas antes de luchar contra su oponente. Rutas de ladrillos y castillos con múltiples peligros serán algunos de los obstáculos que los hermanos tendrán que superar para conseguir su objetivo.', ' Aaron Horvath, Michael Jelenic ', 'Chris Pratt como Mario, Anya Taylor-Joy como la Princesa Peach, Charlie Day como Luigi, Jack Black como Bowser, Keegan-Michael Key como Toad, Seth Rogen como Donkey Kong, Fred Armisen como Cranky Kong, Sebastian Maniscalco como el Capataz Spike, Kevin Michael Richardson como Kamek, Khary Payton como el Rey Pingüino, Eric Bauza como los Koopas Soldados, Rino Romano como el Tío Tony, John DiMaggio como el Tío Arthur, Jessica DiCicco como un Toad Amarillo, Juliet Jelenic como Destello.', '1h 32min', 'COMEDIA, AVENTURA, ANIMACIÓN', '5 Abril 2023', '../assets/mariosinopsis.jpg'),
     new Peliculas(2, "Fast and Furious X", 'Durante numerosas misiones más que imposibles, Dom Toretto y su familia han sido capaces de ser más listos, de tener más valor y de ir más rápido que cualquier enemigo que se cruzara con ellos. Pero ahora tendrán que enfrentarse al oponente más letal que jamás hayan conocido: un terrible peligro que resurge del pasado, que se mueve por una sangrienta sed de venganza y que está dispuesto a destrozar a la familia y destruir para siempre todo lo que a Dom le importa. En "Fast & Furious 5", Dom y su equipo derrotaron al notorio rey brasileño de la droga, Hernán Reyes, y acabaron con su imperio en un puente de Río de Janeiro. Pero no saben que Dante, el hijo de Reyes, lo vio todo y ha pasado los últimos doce años planeando cómo hacérselo pagar a Dom. El complot de Dante desperdigará a la familia de Dom desde Los Ángeles hasta las catacumbas de Roma, de Brasil a Londres, y de Portugal a la Antártida. Aparecerán nuevos aliados y reaparecerán viejos enemigos. Pero todo cambiará cuando Dom descubra que su hijo de ocho años es el objetivo escogido por Dante para saciar su sed de venganza.', 'Louis Leterrier', 'Rita Moreno, Tyrese Gibson, Jason Statham, Michelle Rodriguez, Helen Mirren, Vin Diesel, Brie Larson, Jason Momoa, Sung Kang, Alan Ritchson, Scott Eastwood, Charlize Theron, John Cena, Jordana Brewster, Daniela Melchior, Chris “Ludacris” Bridges, Nathalie Emmanuel', '2h 21min', 'ACCIÓN, THRILLER', '19 Mayo 2023', '../assets/f&fcarrusel.jpg'),
@@ -41,7 +41,7 @@ export class PictureKingdomService {
   buscarPeliculaPorId(id: number): Peliculas | undefined {
     return this.peliculas.find((pelicula) => pelicula.id === id);
   }
-
+ 
   offers = [
     { id: 1, name: "¡BLACK FRIDAY EN PICTURE KINGDOM!", image: '../assets/BANNER-PDF-ENTRADA-BLACK-FRIDAY.jpg', price: "9.99" },
     { id: 2, name: "¡Únete al Reino VIP de Picture Kingdom!", image: '../assets/bannner-promo1.jpg', price: "19.99" },
