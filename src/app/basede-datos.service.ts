53345778cf4b8cf9776f058adfb27ec8a22d93c0
ismaelpenalvafernandez <ismael.penalva@softtek.com>
2023-06-01 13:15:36 +0200

    listarhorarios
    listar horarios

--- src/app/basede-datos.service.ts
+++ src/app/basede-datos.service.ts
@@ -1,45 +1,45 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { Asientos } from './_Modules/Asientos';
-import { Peliculas } from './_Modules/Peliculas';
-import { Usuarios } from './_Modules/Usuarios';
-import { Dias } from './_Modules/Dias';
-import { Horarios } from './_Modules/Horarios';
-@Injectable({
-  providedIn: 'root'
-})
-export class BasedeDatosService {
-  Urlasientos: string = "http://localhost:8080/asientos";
-  Urlpeliculas: string = "http://localhost:8080/peliculas";
-  UrlUsuarios : string = "http://localhost:8080/usuarios";
-  Urlhorarios : string = "http://localhost:8080/horarios";
-  Urldias : string = "http://localhost:8080/dias";
-  constructor(private http: HttpClient) { }
+  import { Injectable } from '@angular/core';
+  import { HttpClient } from '@angular/common/http';
+  import { Observable } from 'rxjs';
+  import { Asientos } from './_Modules/Asientos';
+  import { Peliculas } from './_Modules/Peliculas';
+  import { Usuarios } from './_Modules/Usuarios';
+  import { Dias } from './_Modules/Dias';
+  import { Horarios } from './_Modules/Horarios';
+  @Injectable({
+    providedIn: 'root'
+  })
+  export class BasedeDatosService {
+    Urlasientos: string = "http://localhost:8080/asientos";
+    Urlpeliculas: string = "http://localhost:8080/peliculas";
+    UrlUsuarios : string = "http://localhost:8080/usuarios";
+    Urlhorarios : string = "http://localhost:8080/horarios";
+    Urldias : string = "http://localhost:8080/dias";
+    constructor(private http: HttpClient) { }
 
-  listarasientos(): Observable<Asientos[]> {
-    return this.http.get<Asientos[]>(this.Urlasientos);
+    listarasientos(): Observable<Asientos[]> {
+      return this.http.get<Asientos[]>(this.Urlasientos);
+    }
+
+    listarpeliculas(): Observable<Peliculas[]> {
+      return this.http.get<Peliculas[]>(this.Urlpeliculas);
   }
 
-  listarpeliculas(): Observable<Peliculas[]> {
-    return this.http.get<Peliculas[]>(this.Urlpeliculas);
-}
-
-  listarusuarios(): Observable<Usuarios> {
-  return this.http.get<Usuarios>(this.UrlUsuarios);
-}
-  listarhorarios(): Observable<Horarios> {
-  return this.http.get<Horarios>(this.Urlhorarios);
-}
-  listardiass(): Observable<Dias> {
-  return this.http.get<Dias>(this.Urldias);
-}
-agregarUsuario(usuario: Usuarios): Observable<any> {
-    const url = this.UrlUsuarios+"/agregarUsuario"; 
-    return this.http.post(url, usuario);
-}
-agregarAsiento(asiento: Asientos): Observable<any> {
-  const url = this.Urlasientos+"/agregarAsientos"; 
-  return this.http.post(url, asiento);
-}
-}
+    listarusuarios(): Observable<Usuarios> {
+    return this.http.get<Usuarios>(this.UrlUsuarios);
+  }
+  listarhorarios(): Observable<Horarios[]> {
+    return this.http.get<Horarios[]>(this.Urlhorarios);
+  }
+    listardiass(): Observable<Dias> {
+    return this.http.get<Dias>(this.Urldias);
+  }
+  agregarUsuario(usuario: Usuarios): Observable<any> {
+      const url = this.UrlUsuarios+"/agregarUsuario"; 
+      return this.http.post(url, usuario);
+  }
+  agregarAsiento(asiento: Asientos): Observable<any> {
+    const url = this.Urlasientos+"/agregarAsientos"; 
+    return this.http.post(url, asiento);
+  }
+  }
--- src/app/sinopsis/sinopsis.component.ts
+++ src/app/sinopsis/sinopsis.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Peliculas } from '../_Modules/Peliculas';
 import { PictureKingdomService } from '../picture-kingdom.service';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Ventas } from '../_Modules/Ventas';
 import { BasedeDatosService } from '../basede-datos.service';
+import { Horarios } from '../_Modules/Horarios';
 
 @Component({
   selector: 'app-sinopsis',
@@ -15,7 +16,7 @@ export class SinopsisComponent {
   ide: number = 0;
   pelicula: Peliculas = new Peliculas(0, "", "", "", "", "", "", "", "");
   diasPeliculas: number[] = [];
-  horariosPorDia: { [key: number]: string[] } = {};
+  horariosPorDia: { [key: number]: Horarios[] } = {};
   diaSeleccionado: number = 0;
   horaSeleccionada: string = '';
 
@@ -26,54 +27,53 @@ export class SinopsisComponent {
     private rutes: Router,
     private servicio: BasedeDatosService
   ) {
-    this.peliculas = peliculasS.getPeliculas();
   }
-ngOnInit() {
-  this.ide = this.activarrutas.snapshot.params['id'];
-  this.servicio.listarpeliculas().subscribe(datos => {
-    this.peliculas = datos;
-    const peliculaEncontrada = this.peliculas.filter(pelicula => pelicula.id === +this.ide);
-    if (peliculaEncontrada.length > 0) {
-      this.pelicula = peliculaEncontrada[0];
-      this.peliculasS.vaciarVentas();
-      this.generarFechas();
-      console.log(this.ide);
-    } else {
-      // Aquí puedes manejar el caso de que no se encuentre ninguna película con el ID proporcionado.
-    }
-  });
-}
+  ngOnInit() {
+    this.ide = this.activarrutas.snapshot.params['id'];
+    this.servicio.listarpeliculas().subscribe(datos => {
+      this.peliculas = datos;
+      const peliculaEncontrada = this.peliculas.filter(pelicula => pelicula.id === +this.ide);
+      if (peliculaEncontrada.length > 0) {
+        this.pelicula = peliculaEncontrada[0];
+        this.peliculasS.vaciarVentas();
+        this.generarFechas();
+        console.log(this.ide);
+      } else {
+        // Aquí puedes manejar el caso de que no se encuentre ninguna película con el ID proporcionado.
+      }
+    });
+  }
 
   generarFechas() {
     const fechaActual = new Date();
     this.diasPeliculas = Array.from({ length: 6 }, (_, i) => i);
-
-    const opcionesFecha = { weekday: 'short', month: 'long', day: 'numeric' };
-
-    this.horariosPorDia = {
-      0: ["16:00 PM", "17:00 PM", "18:00 PM", "20:00 PM", "22:00 PM", "24:00 PM"],
-      1: ["17:00 AM", "18:00 PM", "19:00 PM", "20:00 PM"],
-      2: ["17:00 AM", "19:00 PM", "20:00 PM", "21:00 PM"],
-      3: ["16:00 AM", "18:00 PM", "20:00 PM"],
-      4: ["18:00 PM", "20:00 PM", "22:00 PM"],
-      5: ["16:00 AM", "18:00 PM", "20:00 PM", "22:00 PM", "24:00 PM"]
-    };
+  
+    this.servicio.listarhorarios().subscribe((horarios: Horarios[]) => {
+      this.horariosPorDia = horarios.reduce((acumulador: { [key: number]: Horarios[] }, horario: Horarios) => {
+        if (!acumulador[horario.diaid]) {
+          acumulador[horario.diaid] = [];
+        }
+        acumulador[horario.diaid].push(horario);
+        return acumulador;
+      }, {});
+    });
   }
+  
+  
 
   cambiarDiaSeleccionado(dia: number) {
-
     this.diaSeleccionado = dia;
-
   }
 
-  getHorariosPorDia(dia: number): string[] {
-
+  getHorariosPorDia(dia: number): Horarios[] {
     return this.horariosPorDia[dia] || [];
   }
+
   obtenerHorariosPorDiaString(dia: number): string {
     const horarios = this.getHorariosPorDia(dia);
-    return horarios.join(', ');
+    return horarios.map(horario => horario.hora_inicio).join(', ');
   }
+
   obtenerFechaActual(dia: number): string {
     const fechaActual = new Date();
     fechaActual.setDate(fechaActual.getDate() + dia);
@@ -84,12 +84,11 @@ ngOnInit() {
     };
     return fechaActual.toLocaleDateString('es-ES', opcionesFecha);
   }
-ActualizarArray(hora: string){
-  this.horaSeleccionada = hora;
-  this.peliculasS.rellenarVentas({PeliculaID:this.ide})
-  this.peliculasS.rellenarVentas({DiaID:this.obtenerFechaActual(this.diaSeleccionado)})
-  this.peliculasS.rellenarVentas({HoraID: this.horaSeleccionada});
 
-}
-
+  ActualizarArray(hora: Horarios) {
+    this.horaSeleccionada = hora.hora_inicio;
+    this.peliculasS.rellenarVentas({ PeliculaID: this.ide });
+    this.peliculasS.rellenarVentas({ DiaID: this.obtenerFechaActual(this.diaSeleccionado) });
+    this.peliculasS.rellenarVentas({ HoraID: this.horaSeleccionada });
+  }
 }
