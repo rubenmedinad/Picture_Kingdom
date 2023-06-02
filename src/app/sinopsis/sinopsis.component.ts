79447128f29ac657e8e3c8533f0857aa3bf920d0
ismaelpenalvafernandez <ismael.penalva@softtek.com>
2023-06-02 11:10:46 +0200

    sinopsiserrores

--- src/app/sinopsis/sinopsis.component.ts
+++ src/app/sinopsis/sinopsis.component.ts
@@ -5,6 +5,7 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { Ventas } from '../_Modules/Ventas';
 import { BasedeDatosService } from '../basede-datos.service';
 import { Horarios } from '../_Modules/Horarios';
+import { Dias } from '../_Modules/Dias';
 
 @Component({
   selector: 'app-sinopsis',
@@ -15,19 +16,18 @@ export class SinopsisComponent {
   peliculas: Peliculas[] = [];
   ide: number = 0;
   pelicula: Peliculas = new Peliculas(0, "", "", "", "", "", "", "", "");
-  diasPeliculas: number[] = [];
-  horariosPorDia: { [key: number]: Horarios[] } = {};
+  dias: Dias[] = [];
+  horarios: Horarios[]  = [];
   diaSeleccionado: number = 0;
   horaSeleccionada: string = '';
-
 
   constructor(
     private peliculasS: PictureKingdomService,
     private activarrutas: ActivatedRoute,
     private rutes: Router,
     private servicio: BasedeDatosService
-  ) {
-  }
+  ) {}
+
   ngOnInit() {
     this.ide = this.activarrutas.snapshot.params['id'];
     this.servicio.listarpeliculas().subscribe(datos => {
@@ -36,59 +36,22 @@ export class SinopsisComponent {
       if (peliculaEncontrada.length > 0) {
         this.pelicula = peliculaEncontrada[0];
         this.peliculasS.vaciarVentas();
-        this.generarFechas();
-        console.log(this.ide);
-      } else {
-        // Aquí puedes manejar el caso de que no se encuentre ninguna película con el ID proporcionado.
+
       }
     });
-  }
+    this.servicio.listardiass().subscribe(datos => { this.dias = [datos]; });
 
-  generarFechas() {
-    const fechaActual = new Date();
-    this.diasPeliculas = Array.from({ length: 6 }, (_, i) => i);
-  
-    this.servicio.listarhorarios().subscribe((horarios: Horarios[]) => {
-      this.horariosPorDia = horarios.reduce((acumulador: { [key: number]: Horarios[] }, horario: Horarios) => {
-        if (!acumulador[horario.diaid]) {
-          acumulador[horario.diaid] = [];
-        }
-        acumulador[horario.diaid].push(horario);
-        return acumulador;
-      }, {});
+    this.servicio.listarhorarios().subscribe(datos => {
+      this.horarios = datos;
     });
+   
   }
-  
-  
-
-  cambiarDiaSeleccionado(dia: number) {
+  seleccionarDia(dia: number) {
     this.diaSeleccionado = dia;
+    console.log(dia);
   }
-
-  getHorariosPorDia(dia: number): Horarios[] {
-    return this.horariosPorDia[dia] || [];
+  getHorariosPorDia(): Horarios[] {
+    return this.horarios.filter(horario => horario.diaid === this.diaSeleccionado);
   }
-
-  obtenerHorariosPorDiaString(dia: number): string {
-    const horarios = this.getHorariosPorDia(dia);
-    return horarios.map(horario => horario.hora_inicio).join(', ');
-  }
-
-  obtenerFechaActual(dia: number): string {
-    const fechaActual = new Date();
-    fechaActual.setDate(fechaActual.getDate() + dia);
-    const opcionesFecha: Intl.DateTimeFormatOptions = {
-      weekday: 'short',
-      month: 'long',
-      day: 'numeric'
-    };
-    return fechaActual.toLocaleDateString('es-ES', opcionesFecha);
-  }
-
-  ActualizarArray(hora: Horarios) {
-    this.horaSeleccionada = hora.hora_inicio;
-    this.peliculasS.rellenarVentas({ PeliculaID: this.ide });
-    this.peliculasS.rellenarVentas({ DiaID: this.obtenerFechaActual(this.diaSeleccionado) });
-    this.peliculasS.rellenarVentas({ HoraID: this.horaSeleccionada });
-  }
+  
 }
